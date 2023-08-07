79e476685a15acd2ce4cb714f333fc916167d7b2
Emma Goodliffe <emma@piemag.com>
2023-08-07 17:09:20 +0100

    ready to include weeks in commitment batching

--- src/AddBatch.svelte
+++ src/AddBatch.svelte
@@ -4,40 +4,6 @@
   let text = "";
   let json: unknown;
   let status: string[] | true = [""];
-
-  $: {
-    try {
-      json = JSON.parse(text);
-    } catch (err) {
-      status = ["Invalid JSON"];
-    }
-    if (json instanceof Array) {
-      if (json.length) {
-        const validities = json.map(obj => checkCom(obj));
-        const allValid = validities.every(v => v === true);
-        if (allValid) {
-          status = true;
-        } else {
-          status = validities
-            .map((v, i) =>
-              v === true
-                ? ""
-                : v.map(
-                    e =>
-                      `${e.keyword} error at ${
-                        e.instancePath || "/"
-                      } in [${i}]: ${e.message} (${JSON.stringify(e.params)})`
-                  )
-            )
-            .flat();
-        }
-      } else {
-        status = ["Empty"];
-      }
-    } else {
-      status = ["Expected an array"];
-    }
-  }
 </script>
 
 <textarea
@@ -46,6 +12,39 @@
   cols="30"
   rows="10"
   bind:value={text}
+  on:input={() => {
+    try {
+      json = JSON.parse(text);
+      if (json instanceof Array) {
+        if (json.length) {
+          const validities = json.map(obj => checkCom(obj));
+          const allValid = validities.every(v => v === true);
+          if (allValid) {
+            status = true;
+          } else {
+            status = validities
+              .map((v, i) =>
+                v === true
+                  ? ""
+                  : v.map(
+                      e =>
+                        `${e.keyword} error at ${
+                          e.instancePath || "/"
+                        } in [${i}]: ${e.message} (${JSON.stringify(e.params)})`
+                    )
+              )
+              .flat();
+          }
+        } else {
+          status = ["Empty"];
+        }
+      } else {
+        status = ["Expected an array"];
+      }
+    } catch (err) {
+      status = ["Invalid JSON"];
+    }
+  }}
 />
 <!-- TODO: format button -->
 {#if status === true}
