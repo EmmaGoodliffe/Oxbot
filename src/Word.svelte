b6be53ce7812dd7320615d2b53303779bd822067
Emma Goodliffe <emma@piemag.com>
2023-08-25 16:07:41 +0100

    improved definition previewing

--- src/Word.svelte
+++ src/Word.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  // import { getAnalytics } from "firebase/analytics";
   import { isErrorRes } from "../functions/src/types";
   import type { ApiRes, WikiWord } from "../functions/src/types";
   import type { HttpsCallableResult } from "firebase/functions";
@@ -31,19 +30,41 @@
   };
 </script>
 
-<div class="h-64 px-4 py-2 overflow-auto" id="word">
+<div class="max-h-[16rem] px-4 py-2 overflow-auto" id="word">
   {#await wordProm then word}
     {#if word}
-    <div class="overflow-hidden" class:h-12={!showDefinition}>
-      <p>
-        <a href={word.url} target="_blank" class="font-bold">{word.word}</a>
-        <span class="italic">{word.classification}</span>
-      </p>
-      {@html linksToSpans(word.definition)}
-    </div>
+      <div class="overflow-hidden" class:fade={!showDefinition}>
+        <p>
+          <a href={word.url} target="_blank" class="font-bold">{word.word}</a>
+          <span class="italic">{word.classification}</span>
+        </p>
+        {@html linksToSpans(word.definition)}
+      </div>
       {#if !showDefinition}
-        <button class="px-4 py-1 border-2 border-dark-border rounded" on:click={() => (showDefinition = true)}>...</button>
+        <div class="group/ellipsis">
+          <div class="ellipsis">
+            <button on:click={() => (showDefinition = true)}>...</button>
+          </div>
+        </div>
       {/if}
     {/if}
   {/await}
 </div>
+
+<style lang="postcss">
+  .fade {
+    @apply h-36;
+    mask-image: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+  }
+  .ellipsis {
+    @apply flex flex-1 justify-between items-center;
+  }
+  .ellipsis button {
+    @apply mx-2 px-4 py-1 border-2 border-light-bg group-hover/ellipsis:border-light-ui rounded;
+  }
+  .ellipsis::before,
+  .ellipsis::after {
+    content: "";
+    @apply flex-1 h-1 bg-light-bg group-hover/ellipsis:bg-light-ui transition;
+  }
+</style>
